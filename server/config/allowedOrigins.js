5160d5b7da9c756c9f04ab1b84a9c1162274a8c2
Pobi Casaul <phoebecasaul@gmail.com>
2025-03-08 14:26:13 +0800

    update allowed cors options

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,8 @@
 export const allowedOrigins = [
   'http://localhost:5000',
   'http://localhost:5173',
-  'https://react-web-dashboard.up.railway.app/'
+  'http://localhost:5174',
+  'https://react-web-dashboard.up.railway.app',
+  'https://react-web-dashboard.netlify.app',
+  'https://vue-web-dashboard.netlify.app'
 ]

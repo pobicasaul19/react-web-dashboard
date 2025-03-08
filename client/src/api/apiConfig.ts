6fee2b0bc8b36ac8061952f8a8bf92af567aebab
Pobi Casaul <phoebecasaul@gmail.com>
2025-03-08 14:11:53 +0800

    fix 403 error

--- client/src/api/apiConfig.ts
+++ client/src/api/apiConfig.ts
@@ -6,7 +6,7 @@ const axiosConfig = {
 
 // Token injector function to dynamically get the token
 const tokenInjector = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-  const token = localStorage.getItem('auth');
+  const token = JSON.parse(localStorage.getItem('auth') || '{}')?.token || null;
   config.headers = config.headers || ({} as AxiosRequestHeaders);
   if (token) {
     (config.headers as AxiosRequestHeaders)['Authorization'] = `Bearer ${token}`;
